e8916af506de6506af5b54accaf52f950335be92
moofazzel <86169875+moofazzel@users.noreply.github.com>
2021-09-24 04:52:21 +0600

    first commit

---
+++ Traning/random project/js/first _JavaScript.js
@@ -0,0 +1,18 @@
+let x = 50;
+x+=30
+const rony = 100;
+
+let r = 'mofazzel'
+
+
+
+
+document.getElementById('rny').innerHTML = x + rony;
+
+document.getElementById('rnny').innerHTML =rony;
+
+function aamila (){
+  console.log('hello world' );
+}
+
+aamila();
